fb03739f1777887890b50a34c5ca3484d11ae21e
imadyou <imad.yousef79@outlook.com>
2024-06-06 16:14:34 +0200

    adds html, css, js files with the tack calculate method

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById("fiberNumber").addEventListener("input", function () {
+  let fiberNumberInput = document.getElementById("fiberNumber");
+  let value = fiberNumberInput.value;
+
+  if (isNaN(value) || value.trim() === "") {
+    fiberNumberInput.style.color = "red";
+    fiberNumberInput.placeholder = "Enter a valid number";
+  } else {
+    fiberNumberInput.style.color = "";
+    fiberNumberInput.placeholder = "Enter a number";
+  }
+});
+
+function calculateTrackNumber() {
+  let fiberNumberInput = document.getElementById("fiberNumber").value;
+  let resultDiv = document.getElementById("result");
+
+  if (fiberNumberInput === "") {
+    resultDiv.innerHTML = "Please enter a fiber number.";
+    resultDiv.classList.add("red");
+    return;
+  }
+
+  let fiberNumber = parseInt(fiberNumberInput);
+
+  if (isNaN(fiberNumber) || fiberNumber <= 0) {
+    resultDiv.innerHTML = "Please enter a valid positive number.";
+    return;
+  }
+
+  let divisionResult = fiberNumber / 16;
+  let trackNumber;
+
+  if (Number.isInteger(divisionResult)) {
+    trackNumber = divisionResult;
+  } else {
+    trackNumber = Math.ceil(divisionResult);
+  }
+
+  resultDiv.innerHTML = `The fiber track in the cable is ${trackNumber}.`;
+}
